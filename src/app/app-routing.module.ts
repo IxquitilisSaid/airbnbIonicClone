dae4fd822b9c6f6beb2f4820c4a24c7ee4e1336c
Jaime Oliveira <Jaime.Oliveira@softgarden.de>
2020-06-23 09:03:52 +0200

    deps and v9 bump

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'places', pathMatch: 'full' },
-  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
-  { path: 'places', loadChildren: './places/places.module#PlacesPageModule', canLoad: [AuthGuard] },
-  { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule', canLoad: [AuthGuard] },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) },
+  { path: 'places', loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule), canLoad: [AuthGuard] },
+  { path: 'bookings', loadChildren: () => import('./bookings/bookings.module').then(m => m.BookingsPageModule), canLoad: [AuthGuard] },
 ];
 
 @NgModule({
--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -12,11 +12,11 @@ const routes: Routes = [
         children: [
           {
             path: '',
-            loadChildren: './discover/discover.module#DiscoverPageModule'
+            loadChildren: () => import('./discover/discover.module').then(m => m.DiscoverPageModule)
           },
           {
             path: ':placeId',
-            loadChildren: './discover/place-detail/place-detail.module#PlaceDetailPageModule'
+            loadChildren: () => import('./discover/place-detail/place-detail.module').then(m => m.PlaceDetailPageModule)
           }
         ]
       },
@@ -25,19 +25,19 @@ const routes: Routes = [
         children: [
           {
             path: '',
-            loadChildren: './offers/offers.module#OffersPageModule'
+            loadChildren: () => import('./offers/offers.module').then(m => m.OffersPageModule)
           },
           {
             path: 'new',
-            loadChildren: './offers/new-offer/new-offer.module#NewOfferPageModule'
+            loadChildren: () => import('./offers/new-offer/new-offer.module').then(m => m.NewOfferPageModule)
           },
           {
             path: 'edit/:placeId',
-            loadChildren: './offers/edit-offer/edit-offer.module#EditOfferPageModule'
+            loadChildren: () => import('./offers/edit-offer/edit-offer.module').then(m => m.EditOfferPageModule)
           },
           {
             path: ':placeId',
-            loadChildren: './offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule'
+            loadChildren: () => import('./offers/offer-bookings/offer-bookings.module').then(m => m.OfferBookingsPageModule)
           }
         ]
       },
